ef8a4b5614d62733ee5ad834bd5f21960f8c82aa
Simo Ahava <simo.s.ahava@gmail.com>
2019-06-05 13:08:43 +0300

    Update template to use datastore

--- controllers/template.js
+++ controllers/template.js
@@ -3,25 +3,30 @@ const model = require('../models/template-db');
 const createError = require('http-errors');
 const router = express.Router();
 const gtmTplParser = require('../helpers/gtm-custom-template-parser');
-                       
+
 router.get('/:id/:name', async (req, res, next) => {
   try {
-    const id = req.params.id,
-        name = req.params.name;
+    const id = req.params.id;
+
+    // If invalid ID, throw 404
+    if (isNaN(parseInt(id, 10))) {
+      next(createError(404));
+      return;
+    }
 
     // Fetch item that matches ID
-    const result = await model.read(id);
+    const [result] = await model.read(id);
 
     // If no such item exists
-    if (result.length === 0) {
+    if (!result) {
       next(createError(404));
       return;
     }
 
     // Compile template object
-    const template = result[0];
-    const parsed_tpl = gtmTplParser.parseTemplate(template.json,"json");
-     
+    const template = result;
+    const parsed_tpl = gtmTplParser.parseTemplate(template.json, "json");
+
     if (parsed_tpl) {
       template.logo = parsed_tpl.info.brand.thumbnail;
       template.contexts = parsed_tpl.info.containerContexts.join(', ');
@@ -30,29 +35,27 @@ router.get('/:id/:name', async (req, res, next) => {
       template.type = parsed_tpl.info.type;
       template.permissions = parsed_tpl.permissions;
     }
-    
-      
     template.views += 1;
 
     // Increment template views
-    await model.updateViews(id, template.views);
+    await model.update(id, template);
 
     // Render dataLayer and page
     const dataLayer = {
       event: 'datalayer-initialized',
       page: {
         type: 'custom template page',
-        title: name + ' Custom Template'
+        title: template.displayName + ' Custom Template'
       },
       template: parsed_tpl
-    };  
- 
+    };
+
     res.render('template', {
         title: dataLayer.page.title,
         dataLayer: dataLayer,
         template
     });
- 
+
   } catch(err) {
     next(err);
   }

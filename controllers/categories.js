cdda2ff01cc0eef1eef0537543cfbc932842caa1
Simo Ahava <simo.s.ahava@gmail.com>
2019-06-05 13:06:36 +0300

    Update categories to query templates datastore

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,68 +1,51 @@
 const express = require('express');
-const model = require('../models/categories-db');
+const model = require('../models/template-db');
 const createError = require('http-errors');
 const router = express.Router();
+const {categories} = require('../helpers/datastore-schema');
 
-router.get('/', async (req, res, next) => {
+router.get('/', async (req, res) => {
+  const dataLayer = {
+    page: {
+      type: 'categories listing page',
+      title: 'Categories - GTMs Templates'
+    },
+    categories: categories
+  };
+
+  res.render('categories', {
+    title: dataLayer.page.title,
+    dataLayer: dataLayer,
+    categories: categories
+  });
+});
+
+router.get('/:category/', async (req, res, next) => {
   try {
-    // Grab All Categories
-    const result = await model.list();
-
+    const category = req.params.category;
+    // Grab All Templates
+    const result = await model.listByCategory(category);
     // If no such item exists
     if (result.length === 0) {
       next(createError(404));
       return;
     }
-      
-    // Compile categories object
-    const categories = result;
-    // Render dataLayer and page
-    const dataLayer = {
-      page: {
-        type: 'categories listing page',
-        title: 'Categories - GTMs Templates'
-      },
-      categories: categories
-    };
-    
-    res.render('categories', {
-      title: dataLayer.page.title,
-      dataLayer: dataLayer,
-      categories: categories
-    });
-  } catch(err) {
-    next(err);
-  }
-});
-
-router.get('/:id/:slug', async (req, res, next) => {
-  try {      
-    const id = req.params.id;
-    const slug = req.params.slug;
-    // Grab All Templates
-    const result = await model.getTemplatesByCategoryId(id);
-    // If no such item exists
-    if (result.length === 0) {
-      next(createError(404));
-      return;
-    }      
     // Compile categories object
     const templates = result;
     // Render dataLayer and page
     const dataLayer = {
       page: {
         type: 'templates listing page',
-        title: 'Templates by Category: ' + slug +' GTMs Templates',
-        category: slug
+        title: 'Templates by Category: ' + category +' GTMs Templates',
+        category: category
       },
       templates: templates
     };
-    
     res.render('category', {
       title: dataLayer.page.title,
       dataLayer: dataLayer,
       templates: templates,
-      category: slug
+      category: category
     });
   } catch(err) {
     next(err);

7593b8695773f01f6d096165ca45e2b5c415c374
thyngster <thyngster@gmail.com>
2019-06-05 21:21:55 +0200

    Added Logo displaying on Home listing

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const model = require('../models/template-db');
 const router = express.Router();
+const gtmTplParser = require('../helpers/gtm-custom-template-parser');
 
 
 /* GET home page. */
@@ -16,11 +17,28 @@ router.get('/', async (req, res, next) => {
       event: 'datalayer-initialized',
       page: {type: 'home page', title: 'Home - GTM Templates'}
     };
+    const parsed_tpl = gtmTplParser.parseTemplate(rows[0].json, "json");
+
+/*    if (parsed_tpl) {
+      template.logo = parsed_tpl.info.brand.thumbnail;
+      template.contexts = parsed_tpl.info.containerContexts.join(', ');
+      template.displayName = parsed_tpl.info.displayName;
+      template.description = parsed_tpl.info.description;
+      template.type = parsed_tpl.info.type;
+      template.permissions = parsed_tpl.permissions;
+    }
+    */
+    rows.forEach(function(e){
+          const parsed_tpl = gtmTplParser.parseTemplate(rows[0].json, "json");
+          e.logo = parsed_tpl.info.brand.thumbnail
+    });  
+    //console.log(parsed_tpl);      
     res.render('index', {
       title: dataLayer.page.title,
       dataLayer: dataLayer,
       templates: rows
     });
+      
   } catch(err) {
     next(err);
   }

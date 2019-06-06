2868660a6203f2a1d5205d6496d75df045b8ac0d
Simo Ahava <simo.s.ahava@gmail.com>
2019-06-06 10:42:05 +0300

    Refactor template.js

--- controllers/template.js
+++ controllers/template.js
@@ -3,44 +3,28 @@ const model = require('../models/template-db');
 const createError = require('http-errors');
 const router = express.Router();
 const gtmTplParser = require('../helpers/gtm-custom-template-parser');
-const categories_details = {
-    'analytics': {
-        name: 'Analytics',
-        slug: 'analytics',
-        count: 1
-    },
-    'abtest': {
-        name: 'A/B Tests',
-        slug: 'abtest',
-        count: 1
-    },
-    'pixels': {
-        name: 'Marketing Pixel',
-        slug: 'pixels',
-        count: 1
-    }
-}; 
+const enums = require('../helpers/enum');
+
 router.get('/:id/:name', async (req, res, next) => {
   try {
     const id = req.params.id;
 
     // If invalid ID, throw 404
     if (isNaN(parseInt(id, 10))) {
-      next(createError(404));
+      next(createError(404, 'Invalid template ID!'));
       return;
     }
 
     // Fetch item that matches ID
-    const [result] = await model.read(id);
+    const [template] = await model.read(id);
 
     // If no such item exists
-    if (!result) {
-      next(createError(404));
+    if (!template) {
+      next(createError(404, 'Template doesn\'t exist!'));
       return;
     }
 
     // Compile template object
-    const template = result;
     const parsed_tpl = gtmTplParser.parseTemplate(template.json, "json");
 
     if (parsed_tpl) {
@@ -69,8 +53,9 @@ router.get('/:id/:name', async (req, res, next) => {
     res.render('template', {
         title: dataLayer.page.title,
         dataLayer: dataLayer,
-        categories_list: categories_details,
-        template
+        categories_list: enums.categories,
+        template,
+        permissions: enums.permissions
     });
 
   } catch(err) {

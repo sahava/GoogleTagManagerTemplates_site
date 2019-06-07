f0a9b52e8c41bdeb35e40f4cc72fe18b25b1ba02
Simo Ahava <simo.s.ahava@gmail.com>
2019-06-07 13:14:23 +0300

    Update travis

--- controllers/template.js
+++ controllers/template.js
@@ -28,10 +28,8 @@ router.get('/:id/:name', async (req, res, next) => {
     const parsed_tpl = gtmTplParser.parseTemplate(JSON.parse(JSON.stringify(template)));
 
     // Increment template views
-    if (process.env.NODE_ENV === 'production') {
-      template.views += 1;
-      await model.update(id, template);
-    }
+    template.views += 1;
+    await model.update(id, template);
 
     // Render dataLayer and page
     const dataLayer = {

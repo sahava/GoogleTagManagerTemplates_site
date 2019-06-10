f4470f6b4ca9f881e3445b3fa46dc406a92998ae
thyngster <thyngster@gmail.com>
2019-06-10 23:24:11 +0200

    Added all permissions to enum.js

--- helpers/enum.js
+++ helpers/enum.js
@@ -6,9 +6,17 @@ const categories = {
 
 const permissions = {
   access_globals: 'Accesses Global Variables',
-  inject_script: 'Injects Scripts',
+  get_cookies: 'Reads cookie value(s)',
+  get_referrer: 'Reads Referrer URL',
+  get_url: 'Reads URL',
+  inject_hidden_iframe: 'Injects Hidden Iframes',
+  inject_script: 'Injects Scripts',    
   logging: 'Logs to Console',
-  set_cookies: 'Sets a cookie value'
+  read_data_layer: 'Reads Data Layer', 
+  read_character_set: 'Reads Document Character Set',
+  read_title: 'Reads Document Title',
+  send_pixel: 'Sends Pixels',
+  set_cookies: 'Sets a cookie'
 };
 
 module.exports = {

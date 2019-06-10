21d3f820efaaf316934ad3682541dfc718a329e9
thyngster <thyngster@gmail.com>
2019-06-10 14:37:21 +0200

    added SRP Cards filtering
    Added JS based filtering

--- public/js/site.js
+++ public/js/site.js
@@ -1,1 +1,28 @@
 // CUSTOM PAGE JS
+(function($) {
+    $('.filter').on('click', function() {
+        var tag_types = $('[data-filter-tag-type]:checked').map(function() {
+            return $(this).data('filterTagType');
+        }).get();
+
+        var tag_categories = $('[data-filter-tag-category]:checked').map(function() {
+            return $(this).data('filterTagCategory');
+        }).get();
+
+        if (tag_categories.length > 0 || tag_types.length > 0) {
+            $('.card[data-product-type]').each(function() {
+                var ttype = $(this).data('productType').toLowerCase();
+                var tcat = $(this).data('productCategory');
+
+                if(tag_categories.indexOf(tcat)>-1 && tag_types.indexOf(ttype)>-1){
+                    $(this).show();
+                }else{
+                    $(this).hide();
+                }
+            });
+        } else {
+            $('.card[data-product-type]').show();
+        }
+    });
+}
+)(window.jQuery);

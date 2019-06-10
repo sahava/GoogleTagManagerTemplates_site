bd12a26f73bd983aede87973bc19e074498eaec4
thyngster <thyngster@gmail.com>
2019-06-10 22:52:35 +0200

    Update Template controller

--- controllers/api.template.js
+++ controllers/api.template.js
@@ -2,13 +2,19 @@ const express = require('express');
 const model = require('../models/template-db');
 const createError = require('http-errors');
 const router = express.Router();
-// const url = require('url');
 
 router.get('/tpl/:id', async (req, res, next) => {
   try {
+      
+    // List of allowed referrals for hotlinking protection  
     const allowed_referrers = ["localhost","gtm-templates-com.appspot.com","www.gtmtemplates.com","gtmtemplates.com"];
-        
+
+    // Grab the current referrer hostname  
+    const referer_hostname= (req.headers.referer ? new URL(req.headers.referer).hostname : "");  
+          
     const id = req.params.id;
+    const template_url = '/template/'+id;      
+
     // If invalid ID, throw 404
     if (isNaN(parseInt(id, 10))) {
       next(createError(404, 'Invalid template ID!'));
@@ -22,31 +28,22 @@ router.get('/tpl/:id', async (req, res, next) => {
     if (!template) {
       next(createError(404, 'Template doesn\'t exist!'));
       return;
-    }
+    }    
 
-    // Check no referrer
-    if(!req.headers.referer){        
-        res.writeHead(302, {
-          'Location': '/template/'+template.id+'/'+template.slug
-        });     
+    // Check no referrer Check no-allowed-referrer
+    if(allowed_referrers.indexOf(referer_hostname)===-1){   
+        res.redirect(template_url);
         res.end();  
     }      
-    // Check no-allowed-referrer
-    const referer_hostname= new URL(req.headers.referer).hostname;      
-    if(allowed_referrers.indexOf(referer_hostname)===-1){           
-        res.writeHead(302, {
-          'Location': '/template/'+template.id+'/'+template.slug
-        });     
-        res.end();  
-    }   
-      
+
     // Increment template downloads
     template.downloads += 1;
     await model.update(id, template);
-      
+       
     res.setHeader('Content-Type', 'application/json');
     res.set('Content-Disposition', `attachment;filename=${template.slug}.tpl`);
     res.end(template.json);
+    
 
   } catch(err) {
      next(err);

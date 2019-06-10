3b2eaf1c83f582d6606ed8ab2fb5a402872cdc52
thyngster <thyngster@gmail.com>
2019-06-10 15:11:44 +0200

    hotlinking prevention

--- controllers/api.template.js
+++ controllers/api.template.js
@@ -2,11 +2,13 @@ const express = require('express');
 const model = require('../models/template-db');
 const createError = require('http-errors');
 const router = express.Router();
+// const url = require('url');
 
 router.get('/tpl/:id', async (req, res, next) => {
   try {
+    const allowed_referrers = ["localhost","gtm-templates-com.appspot.com","www.gtmtemplates.com","gtmtemplates.com"];
+        
     const id = req.params.id;
-
     // If invalid ID, throw 404
     if (isNaN(parseInt(id, 10))) {
       next(createError(404, 'Invalid template ID!'));
@@ -22,7 +24,23 @@ router.get('/tpl/:id', async (req, res, next) => {
       return;
     }
 
-   // Increment template downloads
+    // Check no referrer
+    if(!req.headers.referer){        
+        res.writeHead(302, {
+          'Location': '/template/'+template.id+'/'+template.slug
+        });     
+        res.end();  
+    }      
+    // Check no-allowed-referrer
+    const referer_hostname= new URL(req.headers.referer).hostname;      
+    if(allowed_referrers.indexOf(referer_hostname)===-1){           
+        res.writeHead(302, {
+          'Location': '/template/'+template.id+'/'+template.slug
+        });     
+        res.end();  
+    }   
+      
+    // Increment template downloads
     template.downloads += 1;
     await model.update(id, template);
       

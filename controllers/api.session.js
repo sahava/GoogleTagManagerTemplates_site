d61e1e054c79a7a6da38869819d6fa3a27fc48a0
Simo Ahava <simo.s.ahava@gmail.com>
2019-06-15 13:04:05 +0300

    Create session login and logout APIs

--- controllers/api.session.js
+++ controllers/api.session.js
@@ -2,21 +2,32 @@ const express = require('express');
 const router = express.Router();
 const {app} = require('../helpers/firebase');
 
-router.post('/', async (req, res) => {
+router.post('/login/', async (req, res) => {
   try {
     const idToken = req.body.idToken.toString();
 
     // Set session cookie settings
     const expiresIn = 1000 * 60 * 60 * 24 * 5;
-    const options = {maxAge: expiresIn, httpOnly: true};
+    const options = {maxAge: expiresIn, httpOnly: true, secure: process.env.NODE_ENV === 'production'};
 
     const sessionCookie = await app.auth().createSessionCookie(idToken, {expiresIn});
 
     res.cookie('session', sessionCookie, options);
     res.end(JSON.stringify({status: 'success'}));
   } catch (err) {
-    console.log(err);
     res.status(401).send('UNAUTHORIZED REQUEST');
+  }
+});
+
+router.get('/logout/', async (req, res) => {
+  try {
+    const sessionCookie = req.cookies.session || '';
+    res.clearCookie('session');
+    const decodedClaims = app.auth().verifySessionCookie(sessionCookie);
+    await app.auth().revokeRefreshTokens(decodedClaims.sub);
+    res.redirect(301, '/');
+  } catch(err) {
+    res.redirect(301, '/');
   }
 });
 

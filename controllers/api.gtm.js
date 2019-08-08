edc6629fde5ef85757774b8f15cf43d7b57bf1ed
thyngster <thyngster@gmail.com>
2019-08-08 22:36:37 +0200

    build proper api code
    all requests return a json with the following format
    {
        status : STATUSCODE,
        message : ERRORCODE ( if any )
       results: []
    }

--- controllers/api.gtm.js
+++ controllers/api.gtm.js
@@ -10,36 +10,57 @@ router.get('/getAccounts', async (req, res) => {
   try {
     const gtm = google.tagmanager({ version: 'v2', auth: oauth2Client });
     const results = await gtm.accounts.list();
-
-    res.json(results.data.account);
+    res.json({
+      status: 200,
+      results: results.data.account
+    });
   } catch (err) {
-    res.status(401).send('UNAUTHORIZED REQUEST');
+    res.status(401).send({
+      status: 401,
+      message: 'unauthorized request'
+    });
   }
 });
 
 router.get('/getContainers/:accountId?', async (req, res, next) => {
   try {
+    let error = null;
     const accountId = req.params.accountId;
     // If no Account ID, throw 500        
     if (!accountId) {
-      next(createError(500, 'Missing accountId!'));
-      return;
+      res.status(500).send({
+        status: 500,
+        message: 'missing account id'
+      });      
     }
 
     // If invalid ID, throw 500        
-    if (isNaN(parseInt(accountId, 10))) {
-      next(createError(500, 'Invalid accountId!'));
-      return;
+    if (isNaN(accountId, 10)) {
+      res.status(500).send({
+        status: 500,
+        message: 'invalid account id (base 10 number expected)'
+      });  
     }
     try {
       const gtm = google.tagmanager({ version: 'v2', auth: oauth2Client });
       const results = await gtm.accounts.containers.list({ parent: 'accounts/' + accountId });
       res.json(results.data.container);
     } catch (err) {
-      res.status(500).send('SOMETHING WENT WRONG');
+      console.log(err.errors);
+      if(err && err.errors && err.errors[0])
+        error = err.errors[0].message;
+      else
+        error = "something went wrong";
+      res.status(500).send({
+        status: 500,
+        message: error
+      });
     }
   } catch (err) {
-    res.status(401).send('UNAUTHORIZED REQUEST');
+    res.status(401).send({
+      status: 401,
+      message: 'unauthorized request'
+    });
   }
 });
 
@@ -49,36 +70,47 @@ router.get('/getWorkspaces/:accountId?/:containerId?/', async (req, res, next) =
     const containerId = req.params.containerId;
     // If no Account ID, throw 500        
     if (!accountId) {
-      next(createError(500, 'Missing accountId!'));
-      return;
+      res.status(500).send({
+        status: 500,
+        message: 'missing account id'
+      });      
     }
-
     // If invalid ID, throw 500        
-    if (isNaN(parseInt(accountId, 10))) {
-      next(createError(500, 'Invalid accountId!'));
-      return;
+    if (isNaN(accountId, 10)) {
+      res.status(500).send({
+        status: 500,
+        message: 'invalid account id (base 10 number expected)'
+      });  
     }
-
     // If no Container ID, throw 500        
     if (!containerId) {
-      next(createError(500, 'Missing containerId!'));
-      return;
+      res.status(500).send({
+        status: 500,
+        message: 'missing container id'
+      }); 
     }
-
     // If invalid Container ID, throw 500        
-    if (isNaN(parseInt(containerId, 10))) {
-      next(createError(500, 'Invalid containerId!'));
-      return;
+    if (isNaN(containerId, 10)) {
+      res.status(500).send({
+        status: 500,
+        message: 'invalid container id (base 10 number expected)'
+      });  
     }
     try {
       const gtm = google.tagmanager({ version: 'v2', auth: oauth2Client });
       const results = await gtm.accounts.containers.workspaces.list({ parent: 'accounts/' + accountId + '/containers/' + containerId });
       res.json(results.data.workspace);
     } catch (err) {
-      res.status(500).send('SOMETHING WENT WRONG');
+      res.status(500).send({
+        status: 500,
+        message: 'something went wrong'
+      });
     }
   } catch (err) {
-    res.status(401).send('UNAUTHORIZED REQUEST');
+    res.status(401).send({
+      status: 401,
+      message: 'unauthorized request'
+    });
   }
 });
 
@@ -90,42 +122,55 @@ router.get('/installTemplate/:templateId?/:accountId?/:containerId?/:workspaceId
     const workspaceId = req.params.workspaceId;    
     // If no template ID, throw 500        
     if (!templateId) {
-      next(createError(500, 'Missing templateId!'));
-      return;
+      res.status(500).send({
+        status: 500,
+        message: 'missing template id'
+      });        
     }
-
     // If invalid ID, throw 500        
-    if (isNaN(parseInt(templateId, 10))) {
-      next(createError(500, 'Invalid templateId!'));
-      return;
+    if (isNaN(templateId, 10)) {
+      res.status(500).send({
+        status: 500,
+        message: 'invalid template id (base 10 number expected)'
+      }); 
     }
-
     // If no Account ID, throw 500        
     if (!accountId) {
-      next(createError(500, 'Missing accountId!'));
-      return;
+      res.status(500).send({
+        status: 500,
+        message: 'missing account id'
+      });      
     }
-
     // If invalid ID, throw 500        
-    if (isNaN(parseInt(accountId, 10))) {
-      next(createError(500, 'Invalid accountId!'));
-      return;
+    if (isNaN(accountId, 10)) {
+      res.status(500).send({
+        status: 500,
+        message: 'invalid account id (base 10 number expected)'
+      });  
     }
-
     // If no Container ID, throw 500        
     if (!containerId) {
-      next(createError(500, 'Missing containerId!'));
-      return;
+      res.status(500).send({
+        status: 500,
+        message: 'missing container id'
+      }); 
     }
-
     // If invalid Container ID, throw 500        
-    if (isNaN(parseInt(containerId, 10))) {
-      next(createError(500, 'Invalid containerId!'));
-      return;
+    if (isNaN(containerId, 10)) {
+      res.status(500).send({
+        status: 500,
+        message: 'invalid container id (base 10 number expected)'
+      });  
     }
-
+    // If no Container ID, throw 500        
+    if (!workspaceId) {
+      res.status(500).send({
+        status: 500,
+        message: 'missing workspace id'
+      }); 
+    }
     // If invalid workspaceID, throw 500        
-    if (isNaN(parseInt(workspaceId, 10))) {
+    if (isNaN(workspaceId, 10)) {
       next(createError(500, 'Invalid workspaceId!'));
       return;
     }    
@@ -149,9 +194,12 @@ router.get('/installTemplate/:templateId?/:accountId?/:containerId?/:workspaceId
       }];
       res.json(ret);
       // TO-DO TASKS
+      // GET https://www.googleapis.com/tagmanager/v2/+parent/templates
+      // accounts/{account_id}/containers/{container_id}/workspaces/{workspace_id}
       // 1. Get Current Container Custom Template List Names
       // 1.2 If current tempalte exisits, template name should be renamed ( maybe GTM API does take care of this automatically, test it)
       // 2. Do the template install call
+      // 3.1  Is success increamente install counte, return msg to site.
       // Increment template installs
       /*template.installs += 1;
       await model.update(id, template);
@@ -165,7 +213,10 @@ router.get('/installTemplate/:templateId?/:accountId?/:containerId?/:workspaceId
       res.status(500).send('SOMETHING WENT WRONG');
     }
   } catch (err) {
-    res.status(401).send('UNAUTHORIZED REQUEST');
+    res.status(401).send({
+      status: 401,
+      message: 'unauthorized request'
+    });
   }
 });
 module.exports = router;

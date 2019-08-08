2fc085205f2e043e3c458776fa0849f5fb0aae62
thyngster <thyngster@gmail.com>
2019-08-08 17:27:59 +0200

    changed logout redirects to 302 to prevent browser caching

--- controllers/api.session.js
+++ controllers/api.session.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const {oauth2Client} = require('./middleware/google-auth');
+const { oauth2Client } = require('./middleware/google-auth');
 
 router.get('/logout/', async (req, res) => {
   try {
     res.clearCookie('gtoken');
-    res.redirect(301, req.headers.referer);
-  } catch(err) {
-    res.redirect(301, '/');
+    res.redirect(302, req.headers.referer);
+  } catch (err) {
+    res.redirect(302, '/');
   }
 });
 
 router.get('/login/', async (req, res) => {
   try {
-    const {tokens} = await oauth2Client.getToken(req.query.code);
+    const { tokens } = await oauth2Client.getToken(req.query.code);
     oauth2Client.setCredentials(tokens);
     res.cookie('gtoken', tokens.refresh_token);
-    res.end(JSON.stringify({status: 'success'}));
+    res.end(JSON.stringify({ status: 'success' }));
   } catch (err) {
     res.status(401).send('UNAUTHORIZED REQUEST');
   }

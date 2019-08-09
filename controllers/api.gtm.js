70dff9cdfda172b4b463203ec0e20485842a34c6
thyngster <thyngster@gmail.com>
2019-08-09 10:27:34 +0200

    proper template name handling

--- controllers/api.gtm.js
+++ controllers/api.gtm.js
@@ -193,62 +193,53 @@ router.get('/installTemplate/:templateId?/:accountId?/:containerId?/:workspaceId
         });         
       }
       const parsed_tpl = gtmTplParser.parseTemplate(JSON.parse(JSON.stringify(template)));      
-
       let bodyParams = {
         "path": ["accounts",accountId,"containers",containerId,"workspaces",workspaceId].join('/'),
         "accountId": accountId,
         "containerId": containerId,
         "workspaceId": workspaceId,
-        //"templateId": string,
-        "name": parsed_tpl.name,
-        //"fingerprint": string,
-        //"tagManagerUrl": string,
+        // "templateId": string,
+        "name": parsed_tpl.info.displayName,
+        // "fingerprint": string,
+        // "tagManagerUrl": string,        
         "templateData": parsed_tpl.json
       }
       const gtm = google.tagmanager({ version: 'v2', auth: oauth2Client });      
-
       // Grab current workspace custom template names
       const results = await gtm.accounts.containers.workspaces.templates.list({ parent: ["accounts",accountId,"containers",containerId,"workspaces",workspaceId].join('/')});
       const tpls = results.data.template.map(function(e){
         return e.name;
       });
-      // Template name already exists
-      if(tpls.indexOf(bodyParams.name)>-1){        
-        // Check if the template name is already a copied one, if not increment number
-        // All this need refactoring, not happy with the way is coded for now.
-        // Talk with simo about naming format
-        // final __INT , or leading "Copy (INT) "
-        if(bodyParams.name.match(/.+(__[0-9]{1,3})$/)){
-          // This template seems to be already a copy
-          const index = parseInt(bodyParams.name.match(/.+__([0-9]{1,3})$/)[1]);
-          const nextIndex = index+1;
-          bodyParams.name = bodyParams.name.replace("__"+index,"__"+nextIndex);
-        }else{
-          bodyParams.name = bodyParams.name + '__1';  
-        }
-        
-      }
 
+      // TO-DO Refactor
+      //
+      const indexes = tpls.filter(function(e) {
+        var reg = new RegExp('^'+bodyParams.name+'$|'+bodyParams.name+'_import_([0-9]+)$','gi');
+        if(e.match(reg))  return e;    
+      });   
+      indexes.sort();
+      if(indexes.length>1){        
+        bodyParams.name = bodyParams.name+'_import_'+(parseInt(indexes.reverse()[0].split('_').reverse()[0])+1).toString();
+      }else if(indexes.length===1){
+        bodyParams.name = bodyParams.name+'_import_1';
+      }      
+
+      // Rename Template Name
+      bodyParams.templateData = bodyParams.templateData.replace('"displayName": "TEST"','"displayName": "'+bodyParams.name+'"');
       const created = await gtm.accounts.containers.workspaces.templates.create({ 
         parent: ["accounts",accountId,"containers",containerId,"workspaces",workspaceId].join('/'), resource: bodyParams
       });
-      res.status(200).send(created);     
-      // TO-DO TASKS
-      // GET https://www.googleapis.com/tagmanager/v2/+parent/templates
-      // accounts/{account_id}/containers/{container_id}/workspaces/{workspace_id}
-      // 1. Get Current Container Custom Template List Names
-      // 1.2 If current tempalte exisits, template name should be renamed ( maybe GTM API does take care of this automatically, test it)
-      // 2. Do the template install call
-      // 3.1  Is success increamente install counte, return msg to site.
-      // Increment template installs
-      /*template.installs += 1;
-      await model.update(id, template);
-      */
-      /*
-      const gtm = google.tagmanager({ version: 'v2', auth: oauth2Client });
-      const results = await gtm.accounts.containers.list({ parent: 'accounts/' + accountId });
-      res.json(results.data.container);
-      */
+      
+      if(created && created.status && created.status===200){
+        res.json({
+          status: 200,
+          results: [created.data]
+        });       
+        /*template.installs += 1;
+        await model.update(id, template); // USe a copy for the template, to avoid the name to be changed.
+        */      
+      }
+
     } catch (err) {
       //res.status(500).send('SOMETHING WENT WRONG');
       res.status(200).send(err);

e0a775fdf2a2e2a61f4ffd4b802f357bb04ce6dd
thyngster <thyngster@gmail.com>
2019-08-13 13:01:57 +0200

    feed controller + router

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,24 +1,44 @@
 const express = require('express');
-//const model = require('../models/template-db');
-//const createError = require('http-errors');
+const model = require('../models/template-db');
 const router = express.Router();
-/*const gtmTplParser = require('../helpers/gtm-custom-template-parser');
 const enums = require('../helpers/enum');
-const dataLayerHelper = require('../helpers/dataLayer');
-*/
-router.get('/atom', async (req, res, next) => {
+
+const xmlbuilder = require('xmlbuilder');
+router.get('/', async (req, res, next) => {
   try {
-
-
-    // If no such item exists
-    /*if (!template) {
-      next(createError(404, 'Template doesn\'t exist!'));
-      return;
+    const feedType = 'atom';
+    if(feedType==='atom'){
+      const {templates} = await model.list(0, null);      
+      const entries = templates.map(function(tpl){
+        const entryModel = {
+          'title': `${tpl.name}`,
+          'id': `tpl:gtmtemplates.com,${tpl.id}`,
+          'updated': `${new Date(tpl.updated_date).toISOString()}`,
+          'published': `${new Date(tpl.added_date).toISOString()}`,
+          'link' : `https://www.gtmtemplates.com/template/${tpl.id}/${tpl.slug}`,
+          'author': {
+            'name': `${tpl.author}`
+          }
+        };        
+        return entryModel;
+      });
+      const feedObj = { 
+        'feed': {
+          '@xmlns': `http://www.w3.org/2005/Atom`,
+          'title': { '@type': 'text', '#text': 'GTMTemplates - Updates Feed' },
+          'subtitle': { '@type': 'html', '#text': 'Feed for latest updated/added templates' },
+          'updated': `${new Date().toISOString()}`,
+          'rights': `Copyright (c) {${new Date("Y")}, Simo Ahava`,
+          'id': `tpls:gtmtemplates.com`,
+          'entry': [entries]
+        }
+      };
+      const feed = xmlbuilder.create(feedObj, { encoding: 'utf-8' });
+      const xml = feed.end({ pretty: true });
+      // Set Proper Content Type
+      res.set('Content-Type', 'application/atom+xml');
+      res.status(200).send(xml);    
     }
-    */
-
-
-
   } catch(err) {
      next(err);
   }
